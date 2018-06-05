4abf038cb161d3f9df752a15c72abcb98662e0ae
Suman Bandita <sumanbandita@gmail.com>
2018-06-05 15:31:43 -0400

    seeding database

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://my-new-mongoose-user:Suman15%40@ds249530.mlab.com:49530/mongoose-db'
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful'
 };
