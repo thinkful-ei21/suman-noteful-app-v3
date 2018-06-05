c8ee1eb3448a2b58aeadb5d5c9416500ccea9643
Suman Bandita <sumanbandita@gmail.com>
2018-06-05 15:07:26 -0400

    created schema and note model

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  PORT: process.env.PORT || 8080
+  PORT: process.env.PORT || 8080,
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://my-new-mongoose-user:Suman15%40@ds249530.mlab.com:49530/mongoose-db'
 };
