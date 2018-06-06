aeb59fbdd8f5624bd8a5d5031f155b2cc1c78e81
Suman Bandita <sumanbandita@gmail.com>
2018-06-06 17:25:40 -0400

    integration test

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test'
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://dev:dev123@ds251240.mlab.com:51240/noteful-app'
 };
